8fcc75d0e77ec80902bf2a40f4cbaac076106bf5
anticlown322 <karasandrey2005@gmail.com>
2024-10-12 19:40:17 +0300

    fix + feat
    - fix minor bugs with entity creation
    - try to implement integrated db

--- BookmakersOffice/UserMicroservice.WebAPI/Program.cs
+++ BookmakersOffice/UserMicroservice.WebAPI/Program.cs
@@ -3,35 +3,44 @@ using Microsoft.EntityFrameworkCore;
 using UserMicroservice.Business.Services;
 using UserMicroservice.Data;
 using UserMicroservice.Data.Repositories;
+using UserMicroservice.WebAPI.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
 /* Add services to the container. */
+//basic
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
-//builder.Services.AddScoped<ExceptionHandlerMiddleware>();
-builder.Services.AddDbContext<ApplicationDbContext>(options => 
-    options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=UserDB;Trusted_Connection=True;"));
+//database
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(config.GetConnectionString("SqlConnection")));
+
+//custom services
+builder.Services.AddScoped<ExceptionHandlerMiddleware>();
 builder.Services.AddScoped<IUserRepository, DefaultUserRepository>();
 builder.Services.AddScoped<IUserService, DefaultUserService>();
 
+//xml documentation
 builder.Services.AddSwaggerGen(options => {
     var basePath = AppContext.BaseDirectory;
     var xmlPath = Path.Combine(basePath, "UserMicroservice.WebAPI.xml");
     options.IncludeXmlComments(xmlPath);
 });
 
+/* Configure the HTTP request pipeline. */
 var app = builder.Build();
 
-/* Configure the HTTP request pipeline. */
-//app.UseExceptionHandlerMiddleware();
+app.UseExceptionHandlerMiddleware();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-//app.UseHttpsRedirection();
 app.MapControllers();
 
 app.Run();
