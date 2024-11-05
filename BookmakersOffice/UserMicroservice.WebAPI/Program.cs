db37815045f624614073e10d507ccf36806a5151
anticlown322 <karasandrey2005@gmail.com>
2024-11-06 00:23:19 +0300

    feat: implemnt ELK logging to user ms

--- BookmakersOffice/UserMicroservice.WebAPI/Program.cs
+++ BookmakersOffice/UserMicroservice.WebAPI/Program.cs
@@ -1,12 +1,17 @@
-//using UserMicroservice.Middlewares;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
 
 using Kafka.Interfaces;
 using Kafka.Producers;
-using Microsoft.EntityFrameworkCore;
 using UserMicroservice.Business.Services;
 using UserMicroservice.Data;
 using UserMicroservice.Data.Repositories;
-using UserMicroservice.WebAPI.Middlewares;
+
+using Serilog;
+using Serilog.Exceptions;
+using Serilog.Sinks.Elasticsearch;
+
+//using UserMicroservice.WebAPI.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,11 +35,16 @@ builder.Services.AddScoped<IUserService, DefaultUserService>();
 builder.Services.AddScoped<IKafkaProducer, UserKafkaProducer>();
 
 //xml documentation
-builder.Services.AddSwaggerGen(options => {
+builder.Services.AddSwaggerGen(options =>
+{
     var basePath = AppContext.BaseDirectory;
     var xmlPath = Path.Combine(basePath, "UserMicroservice.WebAPI.xml");
     options.IncludeXmlComments(xmlPath);
 });
+
+//logger
+configureLogging();
+builder.Host.UseSerilog();
 
 /* Configure the HTTP request pipeline. */
 var app = builder.Build();
@@ -45,6 +55,37 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.MapControllers();
 
 app.Run();
+
+void configureLogging()
+{
+    var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+    IConfigurationRoot configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{environment}.json", optional: true).Build();
+
+    Log.Logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+        .Enrich.WithExceptionDetails()
+        .WriteTo.Debug()
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+        .Enrich.WithProperty("Environment", environment)
+        .ReadFrom.Configuration(configuration)
+        .CreateLogger();
+}
+
+ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string enviroment)
+{
+    return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfig:Uri"]))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{enviroment.ToLower()}-{DateTime.UtcNow:yyyy-MM}",
+        NumberOfReplicas = 1,
+        NumberOfShards = 2
+    };
+}
